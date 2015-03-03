99fe094ce470f53ec948058def2f23a25e80c6f3
Yuji Kamiya <yuji.kamiya@gree.net>
2015-03-03 17:36:42 +0900

    return N*2 to the client when request is int N

--- src/server.cs
+++ src/server.cs
@@ -3,24 +3,40 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.Threading;
 
 namespace TCPServer
 {
     class Server
     {
-        static void Main(string[] args)
+        private static TcpClient client;
+
+        public static void Main(string[] args)
         {
             IPEndPoint ipAdd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
             TcpListener listener = new TcpListener(ipAdd);
             listener.Start(0);
             Console.WriteLine("start listening at port 8888");
 
-            TcpClient client = listener.AcceptTcpClient();
+            TcpClient c = listener.AcceptTcpClient();
+            TCPServer.Server.client = c;
             Console.WriteLine("client connected");
 
+            Thread thread = new Thread(new ThreadStart(TCPServer.Server.talk));
+            thread.Start();
+
+            Thread.Sleep(30000);
+            Console.WriteLine("press a key to finish");
+            Console.ReadLine();
+            listener.Stop();
+        }
+
+        public static void talk()
+        {
+            TcpClient client = TCPServer.Server.client;
             if (client.Connected)
             {
-                listener.Stop();
+                Console.WriteLine("bbbbbbb");
                 NetworkStream netStream = client.GetStream();
                 StreamReader sReader = new StreamReader(netStream, Encoding.UTF8);
 
@@ -34,12 +50,25 @@ namespace TCPServer
                         break;
                     }
                     Console.WriteLine(str);
+
+                    int num;
+                    byte[] sendBytes;
+                    bool isInt = int.TryParse(str, out num);
+                    if (isInt)
+                    {
+                        int sendNum = num * 2;
+                        sendBytes = Encoding.UTF8.GetBytes(sendNum.ToString());
+                    } else
+                    {
+                        string msg = "you should enter numbers";
+                        sendBytes = Encoding.UTF8.GetBytes(msg.ToString());
+                    }
+                    netStream.Write(sendBytes, 0, sendBytes.Length);
                 } while (!str.Equals("quit"));
+
                 sReader.Close();
                 client.Close();
             }
-            Console.WriteLine("press a key to finish");
-            Console.ReadLine();
         }
     }
 }
