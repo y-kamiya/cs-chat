d71720bd74bf623f3d72a53140df0d01e61e3af7
Yuji Kamiya <yuji.kamiya@gree.net>
2015-03-11 20:45:12 +0900

    synchronize each thread to measure elapsed time

--- src/chatBench.cs
+++ src/chatBench.cs
@@ -19,31 +19,40 @@ namespace TCPClient
             int clientCount = int.Parse(args[0]);
             int maxMessageCount = int.Parse(args[1]);
 
+            WaitHandle[] waitHandles = new WaitHandle[clientCount];
+            Thread[] threads = new Thread[clientCount];
+
             for (int i = 0; i < clientCount; i++)
             {
-                string name = "bench" + i;
-                ChatClient client = new ChatClient(name, clientCount, maxMessageCount);
-                Thread thread = new Thread(new ThreadStart(client.Connect));
-                thread.Start();
+                waitHandles[i] = new ManualResetEvent(false);
+                ChatClient client = new ChatClient(i, clientCount, maxMessageCount, waitHandles);
+                threads[i] = new Thread(new ThreadStart(client.Connect));
+                threads[i].Start();
             }
-            Console.WriteLine("if you want to finish this client, press any key");
-            Console.ReadLine();
+
+            for (int i = 0; i < clientCount; i++)
+            {
+                threads[i].Join();
+            }
         }
     }
 
     class ChatClient
     {
+        private int id;
         private string name;
         private int maxMessageCount;
         private int clientCount;
-        private readonly int waitBeforeQuit = 1000;
+        private WaitHandle[] waitHandles;
         private readonly int waitBetweenSend = 10;
 
-        public ChatClient(string name, int clientCount, int maxMessageCount)
+        public ChatClient(int id, int clientCount, int maxMessageCount, WaitHandle[] waitHandles)
         {
-            this.name = name;
+            this.id = id;
+            this.name = "bench" + id;
             this.clientCount = clientCount;
             this.maxMessageCount = maxMessageCount;
+            this.waitHandles = waitHandles;
         }
 
         public void Connect()
@@ -51,7 +60,7 @@ namespace TCPClient
             TcpClient client = new TcpClient("127.0.0.1", 8888);
             NetworkStream netStream = client.GetStream();
             StreamReader sReader = new StreamReader(netStream, Encoding.UTF8);
-            ManualResetEvent mre = new ManualResetEvent(false);
+            ManualResetEvent mre = (ManualResetEvent) this.waitHandles[this.id];
 
             ThreadStart reader = () => 
             {
@@ -59,7 +68,8 @@ namespace TCPClient
                 {
                     string str = sReader.ReadLine();
                     if (str == "FINISHED") break;
-                    if (str == "hi, " + this.name) { Console.WriteLine("aaaaaaaaaaa"); mre.Set(); }
+                    if (str == "hi, " + this.name) { mre.Set(); }
+                    if (str == "<" + this.name + ">: i = " + (this.maxMessageCount - 1)) { mre.Set(); }
                     Console.WriteLine("receiver: " + this.name + ", " + str);
                 }
             };
@@ -72,7 +82,11 @@ namespace TCPClient
                 this.sendMessage(this.name, client.GetStream());
                 Console.WriteLine("send name: " + this.name);
 
-                mre.WaitOne();
+                // wait all thread are connected
+                WaitHandle.WaitAll(this.waitHandles);
+                mre.Reset();
+
+                // stop watch
                 System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
                 sw.Start();
 
@@ -84,13 +98,15 @@ namespace TCPClient
                     Thread.Sleep(this.waitBetweenSend);
                 }
 
-                Thread.Sleep(this.waitBeforeQuit);
+                WaitHandle.WaitAll(this.waitHandles);
+                sw.Stop();
                 this.sendMessage("/quit", netStream);
 
                 readThread.Join();
-                sw.Stop();
+
+                // display result of time
                 string format = "[{0} * {1}] {2} time: {3}";
-                long elapsedTime = sw.ElapsedMilliseconds - this.waitBeforeQuit - this.waitBetweenSend * this.maxMessageCount;
+                long elapsedTime = sw.ElapsedMilliseconds;
                 Console.WriteLine(format, this.clientCount, this.maxMessageCount, this.name, elapsedTime);
             }
             catch (Exception e)
